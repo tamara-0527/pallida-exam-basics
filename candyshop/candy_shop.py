e7ab1e1ae9482dea7854396575b8a57b48b4658a
tamara-0527 <szigeti.tamara@gmail.com>
2017-10-06 13:01:10 +0200

    feat(candy_shop): creating some elements

--- candyshop/candy_shop.py
+++ candyshop/candy_shop.py
@@ -17,6 +17,43 @@
 # The CandyShop should be represented as string in this format:
 # "Inventory: 3 candies, 2 lollipops, Income: 100, Sugar: 400gr"
 
+class CandyShop(object):
+
+    def __init__(self, candy_type):
+        self.inventory = []
+        self.sugar = 0
+        self.price = 0
+        self.income = 0
+        self.amount_of_sugar = 0
+        self.raise_rate = 0
+        self.candy_type = candy_type
+
+    def create_sweets(self, sweets):
+        self.sweets.append(sweet)
+        return ("Inventory: {}, Income: {}, Sugar: {}".format(lent(self.inventory), self.income, self.sugar))
+
+    def sell(self):
+        pass
+
+    def raise_prices(self):
+        self.price *= self.raise_rate
+
+    def buy_sugar(self):
+        if self.amount_of_sugar > self.sugar:
+            self.amount_of_sugar += self.sugar
+            self.income -= self.price
+
+
+class Sweets(object):
+    def __init__(self):
+        self.sugar = 10
+        self.price = 20
+        self.candy_type == 'sweet'
+        
+        self.sugar = 5
+        self.price = 10
+
+
 candy_shop = CandyShop(300)
 candy_shop.create_sweets("candy")
 candy_shop.create_sweets("candy")
